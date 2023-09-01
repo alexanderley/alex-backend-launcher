59fa67d763c0fe9eaec61a48a51370d302f0db07
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-09-01 13:42:03 +0200

    nodemailer vertification added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
+const nodemailer = require("nodemailer");
+const frontend_URL = require("../frontendKey");
 
 const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
 
@@ -10,7 +12,7 @@ const saltRounds = 10;
 
 
 // POST /auth/signup  - Creates a new user in the database
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async(req, res, next) => {
   const { email, password, name } = req.body;
 
   // Check if email or password or name are provided as empty string 
@@ -34,38 +36,55 @@ router.post('/signup', (req, res, next) => {
   }
 
 
-  // Check the users collection if a user with the same email already exists
-  User.findOne({ email })
-    .then((foundUser) => {
-      // If the user with the same email already exists, send an error response
-      if (foundUser) {
-        res.status(400).json({ message: "User already exists." });
-        return;
-      }
 
-      // If email is unique, proceed to hash the password
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hashedPassword = bcrypt.hashSync(password, salt);
+  try {
+    const foundUser = await User.findOne({ email });
+  
+    if (foundUser) {
+      res.status(400).json({ message: "User already exists." });
+      return;
+    }
+  
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
-      // Create the new user in the database
-      // We return a pending promise, which allows us to chain another `then` 
-      return User.create({ email, password: hashedPassword, name });
-    })
-    .then((createdUser) => {
-      // Deconstruct the newly created user object to omit the password
-      // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
-    
-      // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
-
-      // Send a json response containing the user object
-      res.status(201).json({ user: user });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ message: "Internal Server Error" })
+    const verificationToken = jwt.sign({ email }, process.env.TOKEN_SECRET, {
+      expiresIn: "1d",
     });
+
+    const createdUser = await User.create({ email, password: hashedPassword, name, verificationToken });
+
+    // Nodemailer implementation
+    // password from the email server
+    const EMAIL_PASSWORD = process.env.EMAIL_SECRET;
+
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "alexander.ley.inbox@gmail.com",
+        pass: EMAIL_PASSWORD,
+      },
+    });
+
+    // url that is goin to be send to the user in order to verify their email
+    const verificationLink = `${frontend_URL}/verify/?token=${verificationToken}`;
+    const msg = {
+      from: '"The Express app 👻" <foo@example.com>',
+      to: `${email}`,
+      subject: "Account Verification",
+      text: `Click the following link to verify your account: ${verificationLink}`,
+    };
+
+    await transporter.sendMail(msg);
+  
+    const { email: createdEmail, name: createdName, _id: createdId } = createdUser;
+    const user = { email: createdEmail, name: createdName, _id: createdId,  };
+  
+    res.status(201).json({ user });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 
@@ -128,6 +147,25 @@ router.get('/verify', isAuthenticated, (req, res, next) => {
   // Send back the object with user data
   // previously set as the token payload
   res.status(200).json(req.payload);
+});
+
+router.put("/mail/:token", async (req, res, next) => {
+  const { token } = req.params;
+  const verificationToken = token;
+
+  try {
+    const foundUser = await User.findOne({ verificationToken });
+    if (foundUser) {
+      foundUser.confirmed = true;
+      await foundUser.save();
+      res.status(200).json({ message: "User was found and updated" });
+    }
+    if (!foundUser) {
+      res.status(200).json({ message: "User was NOT found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "User could not be found" });
+  }
 });
 
 
